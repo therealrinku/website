7ae1a906fbe27be24c2d673a3dda9a735278d5ce
therealrinku <rinkunited2032@gmail.com>
2023-11-26 14:18:52 +0545

    design revamped

--- public/script.js
+++ public/script.js
@@ -1,23 +1,24 @@
 const form = document.querySelector("form");
-const body = document.querySelector("body");
+const appBody = document.querySelector(".app-body");
+const app = document.querySelector(".app");
 const commandInput = document.getElementById("command-input");
 const helpSection = document.getElementById("help-section");
+const topElem = document.getElementById("app-top");
 
 //data
 const RESPONSES = {
-  help: "<div><b style='color:coral;'>Commands available</b><p>help - for help</p><p>clear - to clear the terminal</p><p>skills - to see the list of my skills</p><p>linkedin - my linkedin profile</p><p>github - my github profile</p><p>projects - my personal projects</p><p>introduction - my brief introduction</p><p>-----</p></div>",
+  help: "<div><b style='color:coral;'>Available commands</b><p>help - for help</p><p>clear - to clear the terminal</p><p>skills - to see the list of my skills</p><p>linkedin - my linkedin profile</p><p>github - my github profile</p><p>projects - my personal projects</p><p>intro - my brief introduction</p><p>-----</p></div>",
   skills:
-    "<div><b style='color:coral;'>I know these things</b><p>React.js</p><p>Next.js</p><p>HTML,CSS,JS</p>  <p>Vue.js</p> <p>Node.js</p>   <p>Jenkins, Heroku, Netlify, Firebase</p> <p>Postgres, MongoDB</p>   <p style='color:coral;'>Most importantly i have a passion to learn more and improve myself !</p><p>-----</p></div>",
+    "<div><b style='color:coral;'>I have experience in following programming languages</b><p>HTML,CSS,JS</p><p>React.js</p><p>Next.js</p>  <p>Vue.js</p> <p>Node.js</p>  <p>Postgres, MongoDB</p> <p>Python and Django</p> <p>React Native</p>   <p style='color:coral;'>Most importantly I am super passionate about learning new tools and technologies.</p><p>-----</p></div>",
   github:
     "<div><b style='color:coral;'>Here is my github profile</b><br/><a href='https://github.com/therealrinku/' target='_blank'>github.com/therealrinku</a><p>-----</p></div>",
   linkedin:
     "<div><b style='color:coral;'>Here is my linkedin profile</b><br/><a href='https://www.linkedin.com/in/rinkunited/' target='_blank'>linkedin.com/in/rinkunited</a><p>-----</p></div>",
   projects:
-    "<div><b style='color:coral;'>Here are some of my personal projects</b><br/><a href='https://uns0cial.web.app/' target=_blank'>Uns0cial(social media app)</a><br/><a href='https://taskyoxx.web.app/' target='_blank'>Taskyoxx(todo app)</a><p>-----</p></div>",
+    "<div><b style='color:coral;'>Here are some of my personal projects</b><br/><a href='https://robosocial.web.app/' target=_blank'>Robosocial(social media app)</a><br/><a href='https://taskyoxx.web.app/' target='_blank'>Taskyoxx(todo app)</a> <br/><a href='https://arcshare.vercel.app/' target='_blank'>Arcshare (file sharing app)</a> <p>-----</p></div>",
   introduction:
-    "<div><b style='color:coral;'>Here is my brief introduction</b><br/><p>My name is Rinku Chaudhari and I am from Nepal. <br/>I have 3+ years of experience working on both frontend and backend technologies.  <br/>I have mostly worked on React.js but I have a knowledge of wide variety of programming languages from backend to frontend.</p><p>-----</p></div>",
-  invalidCommand: "<p>Command not available. Enter 'help' for the list of available commands.</p>",
-  ls: "<p>No any files or folders exists in this directory. Type 'help' for the list of available commands.</p><p>-----</p>",
+    "<div><b style='color:coral;'>Here is my brief introduction</b><br/><p>I'm Rinku Chaudhari from Nepal. <br/>I have 3+ years of experience working on both frontend and backend technologies.  <br/>I have mostly worked on React.js but I have a knowledge of wide variety of programming languages from backend to frontend. enter 'skills' for more info</p><p>-----</p></div>",
+  invalidCommand: "<p>Invalid command. Enter 'help' for the list of available commands.</p>",
 };
 
 const COMMANDS_LIST = {
@@ -25,8 +26,7 @@ const COMMANDS_LIST = {
   skills: "skills",
   github: "github",
   linkedin: "linkedin",
-  introduction: "introduction",
-  ls: "ls",
+  intro: "intro",
   projects: "projects",
 };
 
@@ -42,16 +42,13 @@ function WhatToAppend(commandName) {
     case COMMANDS_LIST.linkedin:
       return RESPONSES.linkedin;
 
-    case COMMANDS_LIST.ls:
-      return RESPONSES.ls;
-
     case COMMANDS_LIST.projects:
       return RESPONSES.projects;
 
     case COMMANDS_LIST.skills:
       return RESPONSES.skills;
 
-    case COMMANDS_LIST.introduction:
+    case COMMANDS_LIST.intro:
       return RESPONSES.introduction;
 
     default:
@@ -67,25 +64,62 @@ commandInput.addEventListener("focusout", function () {
 //event listener
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
   const inputField = document.querySelector("input");
+  if (!inputField.value.trim()) return;
+
   const messageDiv = document.createElement("div");
   messageDiv.setAttribute("id", "error-m");
   messageDiv.style.marginBottom = "15px";
+  messageDiv.style.padding = "0 10px";
   helpSection.innerHTML = "";
 
   if (inputField.value === "clear") {
     const items = document.querySelectorAll("#error-m");
     items.forEach((item) => {
-      body.removeChild(item);
+      appBody.removeChild(item);
     });
     return (commandInput.value = "");
   }
 
   const appendHTML = WhatToAppend(inputField.value.toLowerCase());
   messageDiv.innerHTML = appendHTML;
-  body.appendChild(messageDiv);
+  appBody.appendChild(messageDiv);
 
-  body.appendChild(form);
+  appBody.appendChild(form);
   commandInput.focus();
   commandInput.value = "";
+
+  //scroll to the bottom
+  appBody.scrollTop = appBody.scrollHeight;
+});
+
+// -- > dragging magic <---
+let isDragging = false;
+let initialX;
+let initialY;
+let xOffset = 0;
+let yOffset = 0;
+
+topElem.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  initialX = e.clientX - xOffset;
+  initialY = e.clientY - yOffset;
+});
+
+topElem.addEventListener("mouseup", () => {
+  isDragging = false;
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (isDragging) {
+    e.preventDefault();
+    const currentX = e.clientX - initialX;
+    const currentY = e.clientY - initialY;
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    app.style.transform = `translate(${currentX}px, ${currentY}px)`;
+  }
 });
