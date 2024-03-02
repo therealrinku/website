73e6a6cb407b4f65d1a91b20c76bfbccd37bb0cc
therealrinku <rinkunited2032@gmail.com>
2024-03-02 15:02:03 +0545

    chore: ui updates

--- public/script.js
+++ public/script.js
@@ -1,25 +1,29 @@
+/*
+  Rinkerminal
+*/
+
+// all the possible responses
+const RESPONSES = {
+  help: "<div><b style='color:coral;'>Available commands</b><p>help - for help</p><p>clear - to clear the terminal</p><p>skills - to see the list of my skills</p><p>linkedin - my linkedin profile</p><p>github - my github profile</p><p>projects - my personal projects</p><p>intro - my brief introduction</p><p>-----</p></div>",
+  skills:
+    "<div><b style='color:coral;'>I have experience in following programming languages</b><p>HTML,CSS,JS</p><p>React.js</p><p>Next.js</p>  <p>Vue.js</p> <p>Node.js</p>  <p>Postgres, MongoDB</p> <p>Python and Django</p> <p>React Native</p>   <p style='color:coral;'>Most importantly I am super passionate about learning new tools and technologies.</p><p>-----</p></div>",
+  github:
+    "<div><b style='color:coral;'>Here is my github profile</b><br/><a style='text-decoration:underline;' href='https://github.com/therealrinku/' target='_blank'>github.com/therealrinku</a><p>-----</p></div>",
+  linkedin:
+    "<div><b style='color:coral;'>Here is my linkedin profile</b><br/><a style='text-decoration:underline;' href='https://www.linkedin.com/in/rinkunited/' target='_blank'>linkedin.com/in/rinkunited</a><p>-----</p></div>",
+  projects:
+    "<div><b style='color:coral;'>Here are some of my personal projects</b><br/><a style='text-decoration:underline;' href='https://robosocial.web.app/' target=_blank'>Robosocial(social media app)</a><br/><a style='text-decoration:underline;' href='https://taskyoxx.web.app/' target='_blank'>Taskyoxx(todo app)</a> <br/><a style='text-decoration:underline;' href='https://roboshare.vercel.app/' target='_blank'>Roboshare (file sharing app)</a> <p>-----</p></div>",
+  introduction:
+    "<div><b style='color:coral;'>Here is my brief introduction</b><br/><p>I'm Rinku Chaudhari from Nepal. <br/>I have 3+ years of experience working on both frontend and backend technologies.  <br/>I have mostly worked on React.js but I have a knowledge of wide variety of programming languages from backend to frontend. enter 'skills' for more info</p><p>-----</p></div>",
+  invalidCommand: "<p>Invalid command. Enter 'help' for the list of available commands.</p>",
+};
+
 const form = document.querySelector("form");
 const appBody = document.querySelector(".app-body");
 const app = document.querySelector(".app");
 const commandInput = document.getElementById("command-input");
 const helpSection = document.getElementById("help-section");
 const topElem = document.getElementById("app-top");
-
-//data
-const RESPONSES = {
-  help: "<div><b style='color:coral;'>Available commands</b><p>help - for help</p><p>clear - to clear the terminal</p><p>skills - to see the list of my skills</p><p>linkedin - my linkedin profile</p><p>github - my github profile</p><p>projects - my personal projects</p><p>intro - my brief introduction</p><p>-----</p></div>",
-  skills:
-    "<div><b style='color:coral;'>I have experience in following programming languages</b><p>HTML,CSS,JS</p><p>React.js</p><p>Next.js</p>  <p>Vue.js</p> <p>Node.js</p>  <p>Postgres, MongoDB</p> <p>Python and Django</p> <p>React Native</p>   <p style='color:coral;'>Most importantly I am super passionate about learning new tools and technologies.</p><p>-----</p></div>",
-  github:
-    "<div><b style='color:coral;'>Here is my github profile</b><br/><a href='https://github.com/therealrinku/' target='_blank'>github.com/therealrinku</a><p>-----</p></div>",
-  linkedin:
-    "<div><b style='color:coral;'>Here is my linkedin profile</b><br/><a href='https://www.linkedin.com/in/rinkunited/' target='_blank'>linkedin.com/in/rinkunited</a><p>-----</p></div>",
-  projects:
-    "<div><b style='color:coral;'>Here are some of my personal projects</b><br/><a href='https://robosocial.web.app/' target=_blank'>Robosocial(social media app)</a><br/><a href='https://taskyoxx.web.app/' target='_blank'>Taskyoxx(todo app)</a> <br/><a href='https://roboshare.vercel.app/' target='_blank'>Roboshare (file sharing app)</a> <p>-----</p></div>",
-  introduction:
-    "<div><b style='color:coral;'>Here is my brief introduction</b><br/><p>I'm Rinku Chaudhari from Nepal. <br/>I have 3+ years of experience working on both frontend and backend technologies.  <br/>I have mostly worked on React.js but I have a knowledge of wide variety of programming languages from backend to frontend. enter 'skills' for more info</p><p>-----</p></div>",
-  invalidCommand: "<p>Invalid command. Enter 'help' for the list of available commands.</p>",
-};
 
 const COMMANDS_LIST = {
   help: "help",
@@ -30,7 +34,6 @@ const COMMANDS_LIST = {
   projects: "projects",
 };
 
-//utils
 function WhatToAppend(commandName) {
   switch (commandName) {
     case COMMANDS_LIST.help:
@@ -56,12 +59,12 @@ function WhatToAppend(commandName) {
   }
 }
 
-//never lose focus on command input
+// never lose focus on command input
 commandInput.addEventListener("focusout", function () {
   commandInput.focus();
 });
 
-//event listener
+// command submit listener
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -90,11 +93,11 @@ form.addEventListener("submit", (e) => {
   commandInput.focus();
   commandInput.value = "";
 
-  //scroll to the bottom
+  // scroll to the bottom
   appBody.scrollTop = appBody.scrollHeight;
 });
 
-// -- > dragging magic <---
+// drag and drop stuff going on here
 let isDragging = false;
 let initialX;
 let initialY;
